3e47e565580a26f9b408cfc8e09f24e04c71b166
Niko B <mykola.bobrytskyi@globallogic.com>
2019-11-07 21:28:18 +0200

    Update docker docker related artifacts

---
+++ scripts/generate_aws_config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+region=$1
+aws_access_key_id=$2
+aws_secret_access_key=$3
+
+mkdir ./aws
+
+cat << EOF >> ./aws/config
+[default]
+region = $region
+EOF
+
+cat << EOF >> ./aws/credentials
+[default]
+aws_access_key_id = $aws_access_key_id
+aws_secret_access_key = $aws_secret_access_key
+EOF
+
+chmod 600 ./aws/*
